87bfd78c8dd654ecda19505c4ce30b90f1e31b8f
vallathortense <146817520+vallathortense@users.noreply.github.com>
2024-02-15 13:40:33 +0100

    forge_test

--- src/Coinflip.sol
+++ src/Coinflip.sol
@@ -18,7 +18,7 @@ contract Coinflip is Ownable{
     }
 
     /// @notice Checks user input against contract generated guesses
-    /// @param a fixed array of 10 elements which holds the user's guesses. The guesses are either 1 or 0 for heads or tails
+    /// @param Guesses fixed array of 10 elements which holds the user's guesses. The guesses are either 1 or 0 for heads or tails
     /// @return true if user correctly guesses each flip correctly or false otherwise
     function userInput(uint8[10] calldata Guesses) external view returns(bool){
         // TODO: Get the contract generated flips by calling the helper function getFlips()
@@ -33,7 +33,7 @@ contract Coinflip is Ownable{
     }
 
     /// @notice allows the owner of the contract to change the seed to a new one
-    /// @param a string which represents the new seed
+    /// @param NewSeed string which represents the new seed
     function seedRotation(string memory NewSeed) public onlyOwner {
         // TODO: Cast the string into a bytes array so we may perform operations on it
         bytes memory stringToBytes = bytes(NewSeed);
@@ -51,8 +51,8 @@ contract Coinflip is Ownable{
 
 // -------------------- helper functions -------------------- //
     /// @notice This function generates 10 random flips by hashing characters of the seed
-    /// @param No input as only the seed is used for generating the guesses
-    /// @return a fixed 10 element array of type uint8 with only 1 or 0 as its elements
+    /// @dev No input as only the seed is used for generating the guesses
+    /// @return results_array a fixed 10 element array of type uint8 with only 1 or 0 as its elements
     function getFlips() public view returns(uint8[10] memory){
         // TODO: Cast the seed into a bytes array and get its length
         bytes memory stringToBytes = bytes(seed);
@@ -67,7 +67,7 @@ contract Coinflip is Ownable{
         // TODO: Input the correct form for the for loop
         for (uint i = 0; i < 10; i++){
             // Generating a pseudo-random number by hashing together the character and the block timestamp
-            uint randomNum = uint(keccak256(abi.encode(stringInBytes[i*interval], block.timestamp)));
+            uint randomNum = uint(keccak256(abi.encode(stringToBytes[i*interval], block.timestamp)));
             
             // TODO: if the result is an even unsigned integer, record it as 1 in the results array, otherwise record it as zero
             // we look at modulo 2 to see if it is an even or not
